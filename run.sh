d23930eac98c03749e6a98540697b0dd93129cee
Piotr Gulbinowicz <piotr.gulbinowicz@imgw.pl>
2025-02-18 14:02:07 +0100

    --restart unless-stopped

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ cd /root/sensors_api
 docker rm -f sensors_api
 docker image rm ghcr.io/pgulb/sensors_api:main
 docker pull ghcr.io/pgulb/sensors_api:main
-docker run -d --name sensors_api -v ./db.json:/app/db.json:ro ghcr.io/pgulb/sensors_api:main
+docker run -d --restart unless-stopped --name sensors_api -p 3333:3000 -v ./db.json:/app/db.json:ro ghcr.io/pgulb/sensors_api:main
